f3d32404b7e0d9e5446013b4f8b1b6ee7509565a
Sören Beye <github@hypfer.de>
2023-11-03 19:05:24 +0100

    Add missing settings.h

---
+++ settings.h
@@ -0,0 +1,10 @@
+#define IDX_CPS_KEY 0
+#define IDX_CPM_KEY 2
+#define IDX_uSv_KEY 4
+
+#define IDX_CPM 3
+#define IDX_uSv 5
+#define IDX_MODE 6
+
+#define PIN_UART_RX 4 //D2 on Wemos D1 Mini
+#define PIN_UART_TX 13 // UNUSED
